83605f42902ef3d0466c7214b4fc608879da4f72
yt5122 <vtthakkar2@gmail.com>
2022-11-15 21:54:31 +0530

    Console colour

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 //http module 
+const { default: chalk } = require('chalk')
 var http = require('http')
 
-var PORT = 3000
+var PORT = 9898
 const server = http.createServer()
 server.listen(PORT,()=>{
 
-        console.log('Server is running on a port',PORT)
+        console.log(chalk.blue('Server is running on a port',PORT))
 })
